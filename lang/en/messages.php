24e888e98a7a51d95a2d3ca3a11934a4adae38bd
alireza <alireza.salehi13810102@gmail.com>
2023-10-30 13:51:17 +0330

    add lang message files

---
+++ lang/en/messages.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'notFound' => 'not found !',
+    'created' => ':resource created successfully',
+    'updated' => ':resource updated successfully',
+    'deleted' => ':resource deleted successfully',
+    'fetched' => ':resource fetched successfully',
+    'success' => 'successful !',
+    'delete_fail' => 'failed to delete item !',
+    'server_error' => 'server error !',
+    'delete_batch' => ':batch items deleted successfully'
+];
