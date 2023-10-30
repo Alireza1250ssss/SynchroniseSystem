569a6afa55b5c30c6491f7be22462945d07f73df
Maniac <alireza.salehi13810102@gmail.com>
2023-10-30 21:06:03 +0330

    add first-party apis

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group( function () {
-//   Route::apiResource('users',)
+  Route::apiResource('users',UserController::class);
 });
